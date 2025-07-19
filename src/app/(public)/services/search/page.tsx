781cf29d0eca4401fe13cc587eea93357eec00cb
Deepika Selvam <gauchanneelam@gmail.com>
2025-07-19 15:56:30 -0400

    Remove breadcrumbs bar from search page

--- src/app/(public)/services/search/page.tsx
+++ src/app/(public)/services/search/page.tsx
@@ -227,18 +227,7 @@ function SearchResultsPage() {
       {/* Navbar */}
       <Navbar />
 
-      {/* Breadcrumbs */}
-      <div className="bg-gray-50 border-b">
-        <div className="max-w-7xl mx-auto px-4 py-3">
-          <nav className="flex items-center space-x-2 text-sm">
-            <a href="/" className="text-gray-500 hover:text-gray-700">Home</a>
-            <span className="text-gray-400">/</span>
-            <a href="/services" className="text-gray-500 hover:text-gray-700">Services</a>
-            <span className="text-gray-400">/</span>
-            <span className="text-gray-900">Search Results</span>
-          </nav>
-        </div>
-      </div>
+
 
       {/* Search Banner */}
       <SearchBanner 
