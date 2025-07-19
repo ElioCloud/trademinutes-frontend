38e955b34359907eef857509617cf7c79bc926c7
Deepika Selvam <gauchanneelam@gmail.com>
2025-07-18 22:00:24 -0400

    Change dashboard background from gray to white

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -490,7 +490,7 @@ export default function ProfileDashboardPage() {
 
   return (
     <ProtectedLayout>
-      <div className="flex h-screen bg-gray-50">
+      <div className="flex h-screen bg-white">
         {/* Main Content Area */}
         <div className="flex-1 flex flex-col min-w-0">
           {/* Main Content */}
